dc64c0e8cadc9362a6d506fe6f3ad1b291269cf8
lhoju0158 <lhoju0158@gmail.com>
2024-11-12 13:43:21 +0900

    Feat: Modify OCPEnv_1

--- opc.py
+++ opc.py
@@ -2,19 +2,25 @@ import numpy as np
 import gymnasium as gym
 from or_gym.utils import assign_env_config
 from gymnasium import spaces
+import inspect
+
 
 class OCPEnv_1(gym.Env):
     def __init__(self, *args, **kwargs):
+
         # Initialize environment settings based on input configuration
+        self.n_nodes = kwargs.get('n_nodes', 50)
+        self.n_objects = kwargs.get('n_object', 100)
         assign_env_config(self, kwargs)
+        # print(f"n_nodes = {self.n_nodes}")
 
         # Single proxy 
         self.cache_capacity = 1  # Cache capacity per proxy
-        self.bandwidth_capacity = 1  # Bandwidth capacity per proxy
-        self.n_nodes = 50  # Number of proxy nodes
+        # self.bandwidth_capacity = 1  # Bandwidth capacity per proxy
+        # self.n_nodes = 50  # Number of proxy nodes
 
         # Video object
-        self.n_objects = 100  # Number of video objects based on the reference paper
+        # self.n_objects = 100  # Number of video objects based on the reference paper
 
         # Environment
         self.t_interval = 20  # Time interval
@@ -24,7 +30,7 @@ class OCPEnv_1(gym.Env):
         self.mask = True  # Whether to use a mask to filter valid actions
 
         # Action space now allows each object to be partially assigned to multiple nodes (binary choices)
-        self.action_space = gym.spaces.MultiDiscrete([50] * 100)
+        self.action_space = gym.spaces.Discrete(self.n_nodes) # environment's return value # 50
         # self.action_space = gym.spaces.MultiDiscrete([50] * 100)
         
         # suppose that nodes and objectes are matched one by one
@@ -39,18 +45,23 @@ class OCPEnv_1(gym.Env):
         # Initialize valid actions for each object
         # set all actions are valiable
         self.proxy_validity_mask = np.ones((self.n_objects, self.n_nodes), dtype=int)
+        # eternal mask => later change
         
         # Observation space includes action masks, available actions, and state (proxy status + demand)
         if self.mask:
             self.observation_space = spaces.Dict({
                 "action_mask": spaces.Box(0, 1, shape=(self.n_objects, self.n_nodes)),  # Mask for each object's valid actions
                 "proxy_state": spaces.Box(0, 1, shape=(self.n_nodes, 3)),  # State of each proxy node
-                "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3))  # State of each video object
+                "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3)),  # State of each video object
+                "current_step": spaces.Discrete(self.n_objects),
+                # "assingment_state": spaces.
             })
         else:
             self.observation_space = spaces.Dict({
                 "proxy_state": spaces.Box(0, 1, shape=(self.n_nodes, 3)),
-                "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3))
+                "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3)),
+                "current_step": spaces.Discrete(self.n_objects)
+
             })
         # for debugging
         # print(f"In __init__: self.observation_space['action_mask'] shape = {self.observation_space['action_mask'].shape}") 
@@ -65,7 +76,8 @@ class OCPEnv_1(gym.Env):
         self.state = {
             "action_mask": np.ones((self.n_objects, self.n_nodes)),  # Initialize valid actions for each object
             "proxy_state": np.zeros((self.n_nodes, 3)),  # Initialize state for each proxy node
-            "video_state": self.demand[self.current_step]  # Demand for the current step for each video object
+            "video_state": self.demand[self.current_step],  # Demand for the current step for each video object
+            "current_step": self.current_step
         }
         self.assignment = {}  # Record actions taken at each step
        
@@ -117,9 +129,23 @@ class OCPEnv_1(gym.Env):
 
     #     return self.state, reward, done, truncated, {}
 
+    def step(self, action):
+        caller_frame = inspect.stack()[1]
+        caller_file_path = caller_frame.filename
+        caller_function_name = caller_frame.function
     
-    def _STEP(self, actions):
+        print(f"Caller file path: {caller_file_path}")
+        print(f"Caller function name: {caller_function_name}")
+        # Main step function called externally
+        # print(f"step start")
         print(f"================ step start ================")
+
+        return self._STEP(action)
+    
+    def single_step():
+        return 0
+    
+    def _STEP(self, action):
         done = False
         truncated = False
         node_state = self.state["proxy_state"]  # Extract proxy node state
@@ -141,7 +167,7 @@ class OCPEnv_1(gym.Env):
         # print(f"video_state = {object_demand}")
 
         # Iterate over each object and check assignment across multiple nodes
-        for obj_idx, node_idx  in enumerate(actions):
+        for obj_idx, node_idx  in enumerate(action):
             print(f"action start")
             print(f"obj_idx = {obj_idx}, node_idx = {node_idx}")
             # print(f"validation = {self.state['action_mask']}")
@@ -181,6 +207,69 @@ class OCPEnv_1(gym.Env):
         # print(f"self.state[\"action_mask\"] = {self.state['action_mask']}") 
 
         return self.state, reward, done, truncated, {'action_mask': self.state["action_mask"]}
+    # def _STEP(self, action):
+    #     print(f"================ step start ================")
+    #     done = False
+    #     truncated = False
+    #     node_state = self.state["proxy_state"]  # Extract proxy node state
+    #     object_demand = self.state["video_state"]  # Extract demand of video objects for the current step
+    #     # print(f"actions = {actions}")
+    #     # print(f"actions.shape = {actions.shape}") # actions.shape = (100,)
+    #     # n_objects = 100, n_nodes = 50
+        
+    #     # for debugging
+    #     # print(f"self.action_mask.shape = {self.state['action_mask'].shape}")
+    #     # print(f"self.video_state.shape = {self.state['proxy_state'].shape}")
+    #     # print(f"self.proxy_state.shape = {self.state['proxy_state'].shape}")
+    #     # print(f"time ratio (step / step_limit), bandwidth, and storage demand")
+    #     # print(f"self.video_state = {self.state['video_state']}")
+        
+    #     # Initialize reward
+    #     reward = 0
+    #     # print(f"proxy_state = {node_state}")
+    #     # print(f"video_state = {object_demand}")
+
+    #     # Iterate over each object and check assignment across multiple nodes
+    #     for obj_idx, node_idx  in enumerate(action):
+    #         print(f"action start")
+    #         print(f"obj_idx = {obj_idx}, node_idx = {node_idx}")
+    #         # print(f"validation = {self.state['action_mask']}")
+    #         demand = object_demand[obj_idx, 1:]  # Get bandwidth and storage demands for the object
+    #         # bandwidth & storage
+    #         # Calculate the total demand each node would receive based on the current object's allocation
+    #         if all(node_state[node_idx, 1:] + demand <= 1 + self.tol):
+    #             # Allocate demand to the node
+    #             # bandwidth & storage
+    #             if node_state[node_idx, 0] == 0: 
+    #                 node_state[node_idx, 0] = 1  # Activate the node if inactive
+    #             node_state[node_idx, 1:] += demand  # Add demand to node
+
+    #             # Calculate reward
+    #             reward += np.sum(node_state[:, 0] * (node_state[:, 1:].sum(axis=1) - 2))
+
+    #             # Record assignment
+    #             self.assignment[self.current_step] = (obj_idx, node_idx)
+    #             self.update_state(node_state)
+
+    #         else:
+    #             reward -= 1000  # Penalty if allocation is not possible
+    #             done = True
+    #             print(f"done")
+
+    #             break
+
+    #     # Increment step and check if the episode is done
+    #     self.current_step += 1
+    #     if self.current_step >= self.step_limit:
+    #         print(f"=========================== SUCCESS! ===========================")
+    #         done = True  # End the episode if the step limit is exceeded
+
+    #     # Update the state with the new node_state
+    #     self.update_state(node_state)
+    #     # print(f"self.proxy_validity_mask = {self.proxy_validity_mask}")
+    #     # print(f"self.state[\"action_mask\"] = {self.state['action_mask']}") 
+
+    #     return self.state, reward, done, truncated, {'action_mask': self.state["action_mask"]}
 
     def update_state(self, node_state):
         # Update proxy node state and reset action masks for each object
@@ -215,11 +304,7 @@ class OCPEnv_1(gym.Env):
 
     # def generate_demand(self):
     #     return self.generate_demand_random()
-    
-    def step(self, actions):
-        # Main step function called externally
-        # print(f"step start")
-        return self._STEP(actions)
+
 
     def reset(self, **kwargs):
         # Reset the environment to the initial state

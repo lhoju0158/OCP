9702e6956e77bb77eb23f67baae59a658be5a4ff
lhoju0158 <lhoju0158@gmail.com>
2025-01-03 16:54:00 +0900

    Feat: Update opc.py

--- opc.py
+++ opc.py
@@ -7,57 +7,55 @@ import inspect
 
 class OCPEnv_1(gym.Env):
     def __init__(self, *args, **kwargs):
+        # proxy의 bandwidth, storage capacity = 1
+
         self.n_nodes = 50 # proxy의 개수
-        # self.n_objects = 100
-        # 이제 step의 수가 video object의 수가 된다. step을 100으로 생각하기
-        # 이게 아니라 전체 
+
         assign_env_config(self, kwargs)
         self.cache_capacity = 1  # Cache capacity per proxy
         self.t_interval = 20
         self.step_limit = int(60 * 24 / self.t_interval)  # Total steps per day based on time intervals
+        # 총 72번 step_limit
 
         # Environment
-        self.tol = 1e-5  # Tolerance for numerical stability
-        self.seed = 1234  # Initialize random seed
-        self.mask = True  # Whether to use a mask to filter valid actions
+        self.tol = 1e-5  
+        self.seed = 1234  
+        self.mask = True  
 
         self.action_space = gym.spaces.MultiBinary(self.n_nodes)
-        # 객체의 전체 속성과 값을 확인하기
+        # for debuging
         # print(f"Action Space Details: {self.action_space}")
         # print(f"Action Space Type: {type(self.action_space)}")
         # print(f"Action Space Size: {self.action_space.n}")
         # print(f"Action Space Sample: {self.action_space.sample()}")
         # exit(0)
+
+
         self.proxy_validity_mask = np.ones(self.n_nodes)
-        # 영구적인 proxy mask
-        # Observation space includes action masks, available actions, and state (proxy status + demand)
         if self.mask:
             self.observation_space = spaces.Dict({
                 "action_mask": spaces.Box(0, 1, shape=(self.n_nodes,)),  
-                "proxy_state": spaces.Box(0, 1, shape=(self.n_nodes, 3)), 
-                # "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3)),  
+                "proxy_state": spaces.Box(0, 1, shape=(self.n_nodes, 3)), # 현재 할당된 상태
+                "current_video_state": spaces.Box(0,1,shape=(3))
             })
         else:
             self.observation_space = spaces.Dict({
                 "proxy_state": spaces.Box(0, 1, shape=(self.n_nodes, 3)),
-                # "video_state": spaces.Box(0, 1, shape=(self.n_objects, 3)),
+                "current_video_state": spaces.Box(0,1,shape=(3))
             })
 
         self.reset()
 
     def _RESET(self):
-        # Generate demand and initialize state when resetting the environment
-        self.demand = self.generate_demand()  # Generate bandwidth and storage demands for each object
-        self.current_step = 0  # Initialize current step
+        self.demand = self.generate_demand()  
+        self.current_step = 0  
 
         self.state = {
-            "action_mask": np.ones(self.n_nodes),  # Initialize valid actions for each object
-            "proxy_state": np.zeros((self.n_nodes, 3)),  # Initialize state for each proxy node
-            # "video_state": self.demand[self.current_step],  # Demand for the current step for each video object
-            # "current_step": self.current_step
+            "action_mask": np.ones(self.n_nodes), 
+            "proxy_state": np.zeros((self.n_nodes, 3)),  
+            "current_video_state": np.vstack(self.demand[self.current_step])
         }
         self.assignment = {}  # Record actions taken at each step
-       
 
         return self.state, {'action_mask': self.state["action_mask"]}
 
@@ -66,51 +64,134 @@ class OCPEnv_1(gym.Env):
         print(f"================ step start ================")
         return self._STEP(action)
     
-    def single_step():
-        return 0
-    
     def _STEP(self, action):
+        # 여기서 action의 결과는 action_space가 multibinary이니깐 0과 1로 이루어진 50개의 숫자
+        # 여기서 50개의 숫자 중 1인 곳에 현재 step에 해당하는 demand인 bandwidth과 storage를 가진 video를 할당한다
+        print(f"Now agent's action = {action}")
         done = False
         truncated = False
-        node_state = self.state["proxy_state"]  # Extract proxy node state
-        object_demand = self.state["video_state"]  # Extract demand of video objects for the current step
         reward = 0
 
-        # Iterate over each object and check assignment across multiple nodes
-        for obj_idx, node_idx  in enumerate(action):
-            demand = object_demand[obj_idx, 1:]  # Get bandwidth and storage demands for the object
-            # bandwidth & storage
-            # Calculate the total demand each node would receive based on the current object's allocation
-            if all(node_state[node_idx, 1:] + demand <= 1 + self.tol):
-                # Allocate demand to the node
-                # bandwidth & storage
-                if node_state[node_idx, 0] == 0: 
-                    node_state[node_idx, 0] = 1  # Activate the node if inactive
-                node_state[node_idx, 1:] += demand  # Add demand to node
+        current_video_bandwidth = self.state["current_video_state"][1]
+        current_video_storage = self.state["current_video_state"][2]
+        
+        # 일단 몇번째 proxy에 복사해야하는지 알아내기
 
-                # Calculate reward
-                reward += np.sum(node_state[:, 0] * (node_state[:, 1:].sum(axis=1) - 2))
+        target_proxy = np.where(action == 1)  # action에서 값이 1인 index의 집합
+        print(f"target_proxy = {target_proxy}")
 
-                # Record assignment
-                self.assignment[self.current_step] = (obj_idx, node_idx)
-                self.update_state(node_state)
+        # action_space의 결과 1이면 해당 proxy에 copy하고 0이면 그냥 두기
+        if action.ndim != 1 or len(action) != self.n_nodes or not set(action).issubset({0, 1}):
+            # action이 1차원인지 / 50개인지 / 0과 1만 원소로 가지고 있는지를 확인
+            raise ValueError("Invalid action: {}".format(action))
+        
+        # 모두 0으로 action하라고 하면 안된다 -> 이것도 reward 낮게 반환하기
+        if np.size(target_proxy) == 0:
+            reward = -1000
+            done = True
+        
+        # 여기서 하나라도 할당이 안되는게 있으면 바로 reward 낮추고 바로 done하기
+        # 이거 다음 for 구문에 넣으면 안되는 이유: 밖에 있어야 현재 agent가 내린 action_space 전체에 대한 판단이 가능하다
+        # for 구문 안에 넣으면 reward 늘리다가 이후에 낮출 수 있어서 
+        # => 그냥 temp_reward로 하고 true이면 더하지 말기
 
-            else:
-                reward -= 1000  # Penalty if allocation is not possible
-                done = True
-                print(f"done")
+        is_invalidable = False
+        temp_reward = 0
+        
+        # target_proxy에 대해서 bandwidth와 storage 할당하기
+        for single_proxy in target_proxy:
+            proxy_activate = self.state["proxy_state"][single_proxy,0]
+            proxy_bandwidth = self.state["proxy_state"][single_proxy,1]
+            proxy_storage = self.state["proxy_state"][single_proxy,2]
 
+            ## for debug
+            print(f"current single_proxy = {single_proxy}")
+            print(f"proxy_activate = {proxy_activate}")
+            print(f"proxy_bandwidth = {proxy_bandwidth}")
+            print(f"proxy_storage = {proxy_storage}")
+
+            # 불가능하다면 is_invalidable을 True로 바꾸기
+            if 1 + self.tol <current_video_bandwidth + proxy_bandwidth or 1 + self.tol <current_video_storage + proxy_storage:
+                is_invalidable = True
+                print(f"current single_proxy {single_proxy} is not invalidable")
                 break
 
-        # Increment step and check if the episode is done
-        self.current_step += 1
+            # 활성화 하기
+            if proxy_activate == 0:
+                proxy_activate = 1
+            
+            # 이제 요구대로 할당하기
+            proxy_bandwidth+=current_video_bandwidth
+            proxy_storage+=current_video_storage
+
+            # 모두 끝나고 할당 진행하기
+            self.state["proxy_state"][single_proxy,0] = proxy_activate
+            self.state["proxy_state"][single_proxy,1] = proxy_bandwidth
+            self.state["proxy_state"][single_proxy,2] = proxy_storage
+
+        # 이거 끝나고 더하기
+        if is_invalidable:
+            # 불가능한 action을 agent가 선택한 경우
+            reward = -1000
+            done = True
+        else:
+            # 가능한 action만 선택한 경우
+            # temp_reward 더하기
+            reward = temp_reward
+            # done은 아직 그래도 false -> 한 에피소드가 끝낼 경우만 done = True
+
+
+        # 성공적으로 72회를 넘어섰을 때 -> 모든 video obj가 성공적으로 끝냄
         if self.current_step >= self.step_limit:
-            print(f"=========================== SUCCESS! ===========================")
-            done = True  # End the episode if the step limit is exceeded
+            print(f"current episode is done!!!")
+            done = True
+    
+
+        # return 직전에 상태 update하기
+        #### 여기에 ####
+
+        return self.state, reward, done, truncated, {'action_mask': self.state["action_mask"]}
+        # obs, reward, done, truncated, info = env.step(action)
+        # step 함수에 기대되는 반환값
+
+        # # Iterate over each object and check assignment across multiple nodes
+        # for obj_idx, node_idx  in enumerate(action):
+        #     demand = object_demand[obj_idx, 1:]  # Get bandwidth and storage demands for the object
+        #     # bandwidth & storage
+        #     # Calculate the total demand each node would receive based on the current object's allocation
+        #     if all(node_state[node_idx, 1:] + demand <= 1 + self.tol):
+        #         # Allocate demand to the node
+        #         # bandwidth & storage
+        #         if node_state[node_idx, 0] == 0: 
+        #             node_state[node_idx, 0] = 1  # Activate the node if inactive
+        #         node_state[node_idx, 1:] += demand  # Add demand to node
+
+        #         # Calculate reward
+        #         reward += np.sum(node_state[:, 0] * (node_state[:, 1:].sum(axis=1) - 2))
+
+        #         # Record assignment
+        #         self.assignment[self.current_step] = (obj_idx, node_idx)
+        #         self.update_state(node_state)
+
+        #     else:
+        #         reward -= 1000  # Penalty if allocation is not possible
+        #         done = True
+        #         print(f"done")
+
+        #         break
+
+        # Increment step and check if the episode is done
+        # self.current_step += 1
+        # if self.current_step >= self.step_limit:
+        #     print(f"=========================== SUCCESS! ===========================")
+        #     done = True  # End the episode if the step limit is exceeded
 
         # Update the state with the new node_state
-        self.update_state(node_state)
-        return self.state, reward, done, truncated, {'action_mask': self.state["action_mask"]}
+        # self.update_state(node_state)
+        # return self.state, reward, done, truncated, {'action_mask': self.state["action_mask"]}
+        # obs, reward, done, truncated, info = env.step(action)
+        # step 함수에 기대되는 반환값
+
 
     def update_state(self, node_state):
         # Update proxy node state and reset action masks for each object
@@ -118,7 +199,7 @@ class OCPEnv_1(gym.Env):
 
         # Update proxy and video states in the main state
         self.state["proxy_state"] = np.where(node_state > 1, 1, node_state)  # Clip node state to maximum capacity
-        self.state["video_state"] = self.demand[step]  # Get demand for each object for the current step
+        # self.state["video_state"] = self.demand[step]  # Get demand for each object for the current step
         action_mask = np.ones((self.n_objects, self.n_nodes), dtype=int)
 
         # Create valid action masks for each object at the current step
@@ -165,7 +246,25 @@ class OCPEnv_1(gym.Env):
         # Shape will be (n, 3) representing (time ratio, bandwidth demand, storage demand)
         # print(f"demand = {demand}")
         # exit(0)
+        # 여기서 demand는 3개의 index로 이루어진 배열의 집합
+        # 
         return demand
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    ########
+    # 나중에 고치기
+    ########
 
     # def generate_demand_normal(self):
 
